2e0827506a2d5a8b21aa085b546b901b163280da
root <wilokaxy>
2023-05-27 14:28:29 -0700

    Base 10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,13 +4,13 @@
  *
  * Return: Always 0 (success)
  */
-int  main(void)
+int main(void)
 {
 	int i;
-	for (i = 0; i < 10; i++)
 
+	for (i = 0; i < 10; i++)
 	{
-		printf("%d", i);
+		printf("%d\n", i);
 	}
 	return (0);
 }
