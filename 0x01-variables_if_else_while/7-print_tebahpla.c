40c6046f5592ada5ec30efd6c1a8d01fb6668e69
root <wilokaxy>
2023-05-27 19:21:41 -0700

    Reverse the alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * Main - prints the lowercase alphabet in reverse
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char alphabet;
+
+	for ( i = 122; i >= 97; i--)
+	{
+		alphabet = (char) i;
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
