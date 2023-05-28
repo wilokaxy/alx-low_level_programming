189cf7bc9fb6ace08829d1fb62650d4156c005ff
root <wilokaxy>
2023-05-27 18:48:36 -0700

    No Char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 0 to 9
+ *
+ * Returns:Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i =0; i <10; i++)
+	{
+		putchar( i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
