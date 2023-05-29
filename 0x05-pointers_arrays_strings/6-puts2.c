0b58892fb8d8e03e7f7e06dcbac6142fe6c9556b
root <wilokaxy>
2023-05-29 16:19:52 -0700

    Every other line

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * puts2 - prints every other character of a string
+ * @str - input string 
+ * return: no return
+ */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
+}
