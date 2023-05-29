ca01de26c52da0b9190b8d4d23f1e00074dfdd27
root <wilokaxy>
2023-05-29 15:06:04 -0700

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: input string.
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+
+	return len;
+}
