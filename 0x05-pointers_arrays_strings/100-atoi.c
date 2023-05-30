7fbaabd695ea53b0ef0e8ec9c1dfa61cc52d63e2
root <wilokaxy>
2023-05-30 15:45:17 -0700

    convert string to integers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * _atoi - converts string to integer
+ * @s: input string
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1; 
+	int i =0;
+
+	while (s[i] == ' ')
+	{
+		i++;
+	}
+
+	if (s[i] == '+' || s[i] == '-')
+	{
+		if (s[i] == '-')
+		{
+			sign = -1;
+		}
+		i++;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return result * sign;
+}
