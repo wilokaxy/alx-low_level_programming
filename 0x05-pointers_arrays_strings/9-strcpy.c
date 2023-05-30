fe5ddcf1e7c9a31e91c10bbb01c55bbbd8fd92c4
root <wilokaxy>
2023-05-30 15:16:05 -0700

    string copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * _strcpy - copies the string pointed to by src
- * including the terminating null byte, to the 
+ * including the terminating null byte, to the
  * buffer pointed to by dest.
  * @dest: destination 
  * @src: source
