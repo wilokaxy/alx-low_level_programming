809b1a90cade0991467b669f3fa30a03b1faab12
root <wilokaxy>
2023-06-02 12:44:09 -0700

    Comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - program that prints all possible combinations of single-digit numbers.
+ *
+ * Retun: 0 (Succes)
+ */
+int main(void)
+{
+	int i = 0; 
+	
+	while(i < 10)
+	{
+		putchar('0' + i);
+		i++;
+	}
+	putchar('\t');
+	return (0);
+}
