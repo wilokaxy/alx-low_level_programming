7a08c3402d6cc9be1aad2c1aa05d53f94606628d
root <wilokaxy>
2023-06-04 13:52:36 -0700

    Prints alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,11 @@ int main(void)
 {
 	int i=97;
 	char alphabet;
-	for (i =97; i < 122; i++)
+	for (i > 97; i < 123; i++)
 	{
 	 alphabet =(char) i;
 	printf("%c", alphabet);
 	}
+	printf("\n");
 	return 0;
 }
