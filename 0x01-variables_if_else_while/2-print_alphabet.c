027e69bd4c63b55316143dc70c1f2733d2e935b0
root <wilokaxy>
2023-06-04 14:00:09 -0700

    Prints alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-/*
- * main - prints alphabet
+/**
+ * main - program that prints the alphabet in lowercase, followed by a new line
  *
  * return: 0 (success)
  */
 int main(void)
 {
-	int i=97;
+	int i = 97;
 	char alphabet;
+	
 	for (i = 97; i < 123; i++)
 	{
 	 alphabet =(char) i;
 	printf("%c", alphabet);
 	}
 	printf("\n");
-	return 0;
+	return(0);
 }
