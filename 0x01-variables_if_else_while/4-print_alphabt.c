a9514f8c970a6ad43702e52772a7a555856427f1
root <wilokaxy>
2023-06-05 16:22:23 -0700

    Except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 /**
  *
- * main - prints alphabet letters except q and e 
+ * main - prints alphabet letters except q and e
  *
  * Returns 0 (Success)
  */
 int main(void)
 {
     char alphabet;
-
+    
     for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
     {
         if (alphabet != 'e' && alphabet != 'q')
 	{
             putchar(alphabet);
         }
-	putchar('\n');
     }
+
+    putchar('\n);
     return (0);
 }
