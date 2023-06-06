86fa7a93da4b6d14aef5aaeb1bda47e1ae0e2d07
root <wilokaxy>
2023-06-05 17:14:48 -0700

    Comb 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - program that prints all possible combinations of two two-digit numbers from 00 to 99
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int numb1 = 0;
+
+	while (numb1 <= 99)
+	{
+		int numb2 = 0;
+
+		while (numb2 <= 99)
+		{
+			putchar('0' + (numb1 / 10));
+			putchar('0' + (numb1 % 10));
+			putchar(' ');
+			putchar('0' + (numb2 / 10));
+			putchar('0' + (numb2 % 10));
+
+			if (numb1 != 99 || numb2 != 99)
+			{
+				putchar(',');
+				putchar(',');
+			}
+
+			numb2++;
+
+		}
+
+		numb1++;
+
+		putchar('\n');
+		return (0);
+	}
+}
