f5440348d026892278150e3729bcd606a26cb5ad
root <wilokaxy>
2023-06-11 08:59:32 -0700

    Prints alphabet 10X

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		int j = 97;
+		char c;
+
+		while (j <= 122)
+		{
+			c =(char) j;
+			_putchar(c);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}	
+
