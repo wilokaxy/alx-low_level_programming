25e91da59970a00d30aa7a2172d69646f0d07ff4
root <wilokaxy>
2023-06-12 12:57:14 -0700

    Checks for alphabet character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character 
+ * @c: alphabetic character
+ *
+ * Return: 1 if c is letter, lowercase or uppercase, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (( c >= 'a' && c <= 'z') || ( c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
