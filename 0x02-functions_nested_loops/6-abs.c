cec5e359f2f856cb4d939e59a31fb9ea6c8a0a97
root <wilokaxy>
2023-06-14 17:42:16 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * Return: Integer (Success)
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
+
+	
