c6cf04be4c70abb04a23b8b29775e9f902281e39
root <wilokaxy>
2023-06-14 17:25:32 -0700

    Sign Language

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,9 +3,9 @@
  * print_sign - prints the sign of a number
  * @n: number
  *
- * Return: 1 and prints + if n is greater than zero 
- * 	   0 and prints 0 if n is zero 
- * 	   -1 and prints - if n is less than zero
+ * Return: 1 and prints + if n is greater than zero
+ * 	0 and prints 0 if n is zero
+ * 	-1 and prints - if n is less than zero
  */
 
 int print_sign(int n)
