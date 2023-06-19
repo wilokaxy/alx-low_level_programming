2f68ede18c04df61e32c0ac082537ad7d3388a14
root <wilokaxy>
2023-06-19 09:07:18 -0700

    Prints numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ *
+ * Return: numbers 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
