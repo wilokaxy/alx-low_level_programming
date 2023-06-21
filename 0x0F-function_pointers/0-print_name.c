a2e6924911e007725e37410c50cc5df42d121775
root <wilokaxy>
2023-06-21 14:44:32 -0700

    prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name -  prints a name.
+ * @name: name of a person 
+ * @f: function to a pointer
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
