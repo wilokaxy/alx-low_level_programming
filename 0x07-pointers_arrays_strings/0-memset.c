d03793217c038f7dd5dad57c0a5f96ba8881e43c
root <wilokaxy>
2023-06-29 11:16:04 -0700

    0x07: Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,21 @@
-#include <stdio.h>
-
+#include "main.h"
+/**
+ * _memset - fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array with new value for n bytes
+ */
 char *_memset(char *s, char b, unsigned int n)
-	{
-    unsigned int i;
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b;
-    }
-    return s;
-	}
-
-int main()
 {
-    char str[20] = "Hello, World!";
-    printf("Before memset: %s\n", str);
-    _memset(str, '*', 10);
-    printf("After memset: %s\n", str);
-    return 0;
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
 }
+
