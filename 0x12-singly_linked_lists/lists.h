6fce30d0a0863114e1e9cbb5fe4312bfa6bb47ba
wilokaxy <122742204+wilokaxy@users.noreply.github.com>
2023-07-03 21:33:37 +0200

    header files

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,13 @@
 #ifndef _LIST_H_
 #define _LIST_H_
 
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
