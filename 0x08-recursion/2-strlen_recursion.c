d05006952efccf371b7d176e50725148ab4f0e2a
wilokaxy <aaahaabz@gmail.com>
2023-08-02 12:03:04 -0700

    lenght of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
 
 /**
  * _strlen_recursion - returns the length of a string
- * @s: charactes of a string 
+ * @s: charactes of a string
  *
  * Return: 0
  */
@@ -12,7 +12,7 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
-	return 1 + _strlen_recursion(s + 1);
+	return (1 + _strlen_recursion(s + 1));
 }
