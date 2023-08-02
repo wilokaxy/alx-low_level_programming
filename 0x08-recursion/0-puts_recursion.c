8384293ae37632f3492a7885307356cda3ca1b3f
wilokaxy <aaahaabz@gmail.com>
2023-08-02 11:02:50 -0700

    prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: characters in a string 
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if(*s == NULL)
+	{
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
