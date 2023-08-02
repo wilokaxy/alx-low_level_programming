3665a99123c9d73ceb32ed593e8ea1a4d4eb543c
wilokaxy <aaahaabz@gmail.com>
2023-08-02 11:10:18 -0700

    prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,13 +3,13 @@
 
 /**
  * _puts_recursion - function that prints a string
- * @s: characters in a string 
+ * @s: characters in a string
  * Return: 0
  */
 
 void _puts_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
 	{
 		return;
 	}
