db51a063d94ef4efbfa0f516400d11c168b9fbaa
wilokaxy <aaahaabz@gmail.com>
2023-08-05 10:05:37 -0700

    Square of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,18 +10,6 @@
  * Return: integer
  */
 
-int _sqrt_recursion(int n)
-{
-	if(n < 0)
-	{
-		return (-1);
-	}
-	if(n == 0 || n == 1)
-	{
-		return n;
-	}
-}
-
 int _sqrt_helper(int n, int start, int end)
 {
 	if(start > end)
@@ -38,4 +26,16 @@ int _sqrt_helper(int n, int start, int end)
 		return _sqrt_helper(n, mid +  1, end);
 	else 
 		return _sqrt_helper(n, start, mid - 1);
+}
+
+int _sqrt_recursion(int n)
+{
+        if(n < 0)
+        {
+                return (-1);
+        }
+        if(n == 0 || n == 1)
+        {
+                return n;
+        }
 }
